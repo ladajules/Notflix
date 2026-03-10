bf78bd57c9972bde9bf51dc9cbf8451b8492e3dc
Jules Lada <juleslada35@gmail.com>
2026-03-11 01:14:17 +0800

    style: search layout

--- app/src/main/java/com/ladajules/notflix/ui/main/MainActivity.kt
+++ app/src/main/java/com/ladajules/notflix/ui/main/MainActivity.kt
@@ -1,21 +1,62 @@
 package com.ladajules.notflix.ui.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.ladajules.notflix.R
+import com.ladajules.notflix.ui.search.SearchActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavBar: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        initViews()
+        setupListeners()
+    }
+
+    private fun initViews() {
+        bottomNavBar = findViewById(R.id.bottomNavBar)
+    }
+
+    private fun setupListeners() {
+        findViewById<android.view.View>(R.id.searchBar).setOnClickListener {
+            startActivity(Intent(this, SearchActivity::class.java))
         }
+
+        bottomNavBar.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    true
+                }
+                R.id.nav_search -> {
+                    // Open SearchActivity without marking it as selected
+                    startActivity(Intent(this, SearchActivity::class.java))
+                    // Return false to not change selection
+                    false
+                }
+                R.id.nav_downloads -> {
+                    // TODO: Implement downloads screen
+                    true
+                }
+                R.id.nav_more -> {
+                    // TODO: Implement more menu
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // Reset bottom nav to Home when returning to this activity
+        bottomNavBar.selectedItemId = R.id.nav_home
     }
 }
